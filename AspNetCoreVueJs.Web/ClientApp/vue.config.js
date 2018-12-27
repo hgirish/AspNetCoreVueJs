57e87a1f8b2e3c231bee9b5553c14125b21d04a2
Girish <girish@post.com>
2018-12-27 11:22:06 -0800

    Vue app setup

---
+++ AspNetCoreVueJs.Web/ClientApp/vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    devServer: {
+        //https: {
+        //    key: fs.readFileSync('selfsigned.key'),
+        //    cert: fs.readFileSync('localhost.crt'),
+        //    ca: null,
+        //},
+      //  https:true,
+        //public: 'localhost:8080',
+        //host:'localhost',
+        //port: 8081,
+        //disableHostCheck: true,
+        //compress:true,
+    }
+}
