b30017e06e8c12cd4bd3f436d07605700813d8b6
hgirish <girish@post.com>
2018-12-28 17:39:19 -0800

    vue add vuex

--- AspNetCoreVueJs.Web/ClientApp/src/main.js
+++ AspNetCoreVueJs.Web/ClientApp/src/main.js
@@ -5,11 +5,13 @@ import App from './App.vue';
 import router from './router.js';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import store from './store'
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app');
